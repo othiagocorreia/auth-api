3ccde28bb092afd36b2298162306404f5f94a70c
thiago <dasilvacorreiathiago@gmail.com>
2022-03-01 16:04:23 -0300

    code'

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -103,35 +103,58 @@ module.exports = {
             return res.send("You do not have permission to do that")
         }
 
-        const userAlredyAssigned = await prisma.user.findFirst({
-            where: {
-                name: userName
-            }, include: { user_roles: true }
-        })
+       const userHasPermission = await prisma.user.findFirst({where:{name:userName}, include:{
+           user_roles:true
+       }})
 
-        if (userAlredyAssigned.user_roles.role_name != role) {
-            return res.send("User alredy has those permissions")
-        } else {
-            const newUserAssigned = await prisma.user.delete({
-                where: {
-                    name: userName
-                },
-                data: {
-                    user_roles: {
-                        create: {
-                            role_name: role
-                        }
+       if(!userHasPermission){
+           return res.send("User does not exist")
+       }
+
+       const userRole = userHasPermission.user_roles
+       if(userRole[0].role_name != role){
+        return res.send("User does not have this role")
+       } else{
+           const deletedRoleOnUser = await prisma.user_roles.delete({
+                where:{
+                    user_name_role_name:{
+                        role_name:role,
+                        user_name:userName
                     }
                 }
-            })
-
-            return res.send(newUserAssigned)
-        }
+           })
+           return res.send(deletedRoleOnUser)
+       }
+       
+       
     },
     async addNewPeople(req, res){
-        const user = req.userInfo
-        const roles = user.user_roles.role_name
+        //Verifica se o usuário pode
 
-        return res.send(roles)
+        const userName = req.userInfo.name
+        const {name, email, password} = req.body
+        const validPass = await bcrypt.hash(password, 12)
+
+        const existUser = await prisma.user.findFirst({ where: { name:userName }, include: { user_roles: true } })
+        if(!existUser){ return res.send("That occours an error") }
+        const roles = existUser.user_roles
+
+        if(roles.create == false){
+            return res.send("You cannot create users")
+        }
+
+        const newuser = await prisma.user.create({
+            data:{
+                name,
+                email, 
+                password:validPass
+            }
+        })
+
+        if(!newuser){
+            return res.send("User cannot be created")
+        }
+
+        return res.send(newuser)
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res)=>{return res.send({status: 'funciona ⚡'})})
 router.post('/create-users', user.create)
 router.post('/login-users', user.login)
 router.post('/delete-users', user.delete)
-router.get('/read-all-users',authenticated, user.readAll)
+router.get('/read-all-users', authenticated, user.readAll)
 
 router.post('/create-role', role.create)
 router.post('/read-roles', role.read)
@@ -18,4 +18,6 @@ router.post('/delete-roles', authenticated, role.delete)
 
 router.post('/assign-new-user', authenticated, user.assignRole)
 router.post('/delete-role-user', authenticated, user.deniedRoleUser)
+
+router.post('/create-new-user', authenticated, user.addNewPeople)
 module.exports = router
